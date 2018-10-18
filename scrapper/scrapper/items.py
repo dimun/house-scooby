b9b3a4e7c619bdaa4a248fdacbb06a3c1dbde88b
paolagc <estarguars113@gmail.com>
2018-10-18 17:32:42 -0500

    Added items extra validations

--- scrapper/scrapper/items.py
+++ scrapper/scrapper/items.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from scrapy import Item,  Field
-from scrapy.loader.processors import Compose, MapCompose, Join, TakeFirst
+from scrapy.loader.processors import Compose, MapCompose, Join, TakeFirst, Identity
+
+# processing utilities
 import re
+from w3lib.html import remove_tags
 
 # cleaning and extracting utilities
 def strip_spaces(input):
@@ -31,5 +34,17 @@ class PropertyItem(Item):
         input_processor=MapCompose(extract_digits),
         output_processor=TakeFirst()
     )
-    neighborhood = Field()
+    neighborhood = Field(
+        input_processor=MapCompose(strip_spaces),
+        output_processor=Join()
+    )
     status = Field()
+
+    description = Field(
+        input_processor=MapCompose(remove_tags),
+        output_proccesor=Identity()
+    )
+
+    features = Field(
+        
+    )
