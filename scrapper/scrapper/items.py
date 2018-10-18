ab6c934573589badd8e735e91fb05025bc2f62ef
paolagc <estarguars113@gmail.com>
2018-10-18 15:54:42 -0500

    Added basic preprocessing in items definition

--- scrapper/scrapper/items.py
+++ scrapper/scrapper/items.py
@@ -1,15 +1,35 @@
 # -*- coding: utf-8 -*-
 from scrapy import Item,  Field
-from scrapy.loader.processors import Compose, MapCompose, Join
+from scrapy.loader.processors import Compose, MapCompose, Join, TakeFirst
+import re
 
 # cleaning and extracting utilities
-clean_text = Compose(MapCompose(lambda v: v.strip()), Join())   
+def strip_spaces(input):
+    return input.rstrip('\r\n ')
+
+def extract_digits(input):
+    return  re.findall(r'\b\d+\b',input)
 
 class PropertyItem(Item):
-    city = Field()
-    price = Field()
-    bedrooms = Field()
-    bathrooms = Field()
-    surface = Field()
+    city = Field(
+        input_processor=MapCompose(strip_spaces),
+        output_processor=Join()
+    )
+    price = Field(
+        input_processor=MapCompose(extract_digits),
+        output_processor=TakeFirst()
+    )
+    bedrooms = Field(
+        input_processor=MapCompose(extract_digits),
+        output_processor=TakeFirst()
+    )
+    bathrooms = Field(
+        input_processor=MapCompose(extract_digits),
+        output_processor=TakeFirst()
+    )
+    surface = Field(
+        input_processor=MapCompose(extract_digits),
+        output_processor=TakeFirst()
+    )
     neighborhood = Field()
     status = Field()
